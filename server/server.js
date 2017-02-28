39c41d89a06a630722d7b66dd3fb511edbcc57a2
garm_odell <lledoteirag@gmail.com>
2017-02-27 17:50:50 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.post('/', function(req, res) {
+	res.send('POST');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
