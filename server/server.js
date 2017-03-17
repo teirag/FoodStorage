7bfd7b05a79cb1d88dca0bf7e8ac479807045004
Mitchell Sotto <sottochoro@gmail.com>
2017-03-17 11:35:36 -0600

    server.js working!

--- server/server.js
+++ server/server.js
@@ -2,14 +2,49 @@ var express = require('express');
 //const bodyParser = require('body-parser');
 //const peopleRoutes = require('./routes/people');
 var app = express();
+var path = require('path');
+
+app.use(express.static("www"));
+//app.use(express.static("www/html/Family"));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
+app.get('/storage', function (req, res) {
+  res.send('Hello World!')
+});
+
+
+
+
+
+
+app.get('/family', function (req, res) {
+    res.sendFile(path.join(__dirname, '../www/html/Family/', 'family.html'));
+});
+
+
+
+
+
+
+app.get('/recipes', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.get('/budget', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.post('/user', function(req, res) {
+    res.send('Got a POST request on user page');
+});
+
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
+
 
 
 
