6d34c57ec3da0779bb7051c65234ecb7501c76b0
Mitchell Sotto <sottochoro@gmail.com>
2017-03-17 10:26:14 -0600

    server.js changes

--- server/server.js
+++ server/server.js
@@ -1,23 +1,39 @@
 var express = require('express');
-const bodyParser = require('body-parser');
-const peopleRoutes = require('./routes/people');
+//const bodyParser = require('body-parser');
+//const peopleRoutes = require('./routes/people');
 var app = express();
 
-app.use(bodyParser.json());
-
-app.use(express.static('./www'));
-
-/*app.get('/', function (req, res) {
+app.get('/', function (req, res) {
   res.send('Hello World!')
-})*/
-
-app.use('/api', bodyParser.json());
-app.use('/api/people', peopleRoutes);
-
-app.post('/', function(req, res) {
-	res.json(req.body);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+//app.use(bodyParser.json());
+
+//app.use(express.static('./www'));
+
+
+//app.use('/api', bodyParser.json());
+//app.use('/api/people', peopleRoutes);
+//
+//app.post('/', function(req, res) {
+//	res.json(req.body);
+//});
+//
+//app.listen(3000, function () {
+//  console.log('Example app listening on port 3000!')
+//})
