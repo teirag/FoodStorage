c32d1541ba9864cf21da07793115b426c4df6d07
garm_odell <lledoteirag@gmail.com>
2017-03-29 17:23:07 -0600

    added a bunch of mongo stuff

--- server/database.js
+++ server/database.js
@@ -1,13 +1,94 @@
 var MongoClient = require('mongodb').MongoClient;
 
+const dbName = foodStorage;
 module.exports = function(dbName) {
     var url = 'mongodb://localhost:27017/' + dbName;
     return MongoClient.connect(url);
 };
+// we use promises for our insert/update/other functions with Mongo so that they either pass and go through if connected, or they fail and nothing bad happens
+
 
 // fileA.js (different file referencing this one)
 //const db = require('./database');
 //const connPromise = db('myDb');
 //
 //connPromise.then(db => db.insert());
+//dbpromise.then
 //connPromise.then(db => db.find());
+
+
+//Mongo QuickStart stuff
+/*var MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+    
+});
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+var updateDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });  
+}
+
+var removeDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Delete document where a is 3
+  collection.deleteOne({ a : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });    
+}
+
+var indexCollection = function(db, callback) {
+  db.collection('documents').createIndex(
+    { "a": 1 },
+      null,
+      function(err, results) {
+        console.log(results);
+        callback();
+    }
+  );
+};*/
